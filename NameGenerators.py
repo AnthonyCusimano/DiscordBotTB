88882a44389c1ba67f7dbc70b0c8fe5098bac14d
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-01-29 18:15:00 -0500

    Update NameGenerators.py
    "type two" tavern names added w/ redundancy protection ("River and River")
    currently new lines at the end of the .txt aren't being ignored
    townAlias skeleton added

--- NameGenerators.py
+++ NameGenerators.py
@@ -11,9 +11,22 @@ tavernTwo = tavernLines[1].split(',')
 
 #
 def FormTavernName():
-    T_Return = choice(tavernOne)
-    T_Return += ' '
-    T_Return += choice(tavernTwo)
+    T_TypeRando = randrange(0, 50)
+    T_Return = ""
+    if T_TypeRando > 25:
+        T_Return = choice(tavernOne)
+        T_Return += ' '
+        T_Return += choice(tavernTwo)
+    # type two
+    else:
+        T_Return = choice(tavernTwo)
+        # making sure you can't have a "wagon and wagon"
+        T_ReturnProtection = T_Return
+        T_ReturnTwo = choice(tavernTwo)
+        while T_ReturnTwo == T_ReturnProtection:
+            T_ReturnTwo = choice(tavernTwo)
+        T_Return += ' and '
+        T_Return += choice(tavernTwo)
     return T_Return
 
 
@@ -24,3 +37,8 @@ class NameGenerators(commands.Cog):
     @commands.command(name="tavern", aliases=["tavernName", "inn", "innName"])
     async def tavern(self, ctx):
         await ctx.channel.send(FormTavernName())
+
+    #
+    @commands.command(name="townAlias", aliases=["town"])
+    async def townAlias(self, ctx):
+        await ctx.channel.send("under construction command")
