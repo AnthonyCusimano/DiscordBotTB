0d13a729091e65a4dafdd1e012619b9c37f7a6e6
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-02-23 22:09:04 -0500

    Update SpellBookGenerators.py
    changed name of the list to reflect that these are all 1st level spells
    also added list skeletons for 5e sorcerer spells and 3.5 wizard spells
    skeleton for 5e wizard book creator started

--- SpellBookGenerators.py
+++ SpellBookGenerators.py
@@ -5,18 +5,21 @@ from discord.ext import commands
 
 # magic user spells
 # remember everyone starts with Read Magic so reroll on
-LOTFPCoreMUSpells = ["Bookspeak", "Charm Person", "Comprehend Languages / Obscure Languages", "Detect Magic",
+LOTFPCoreMU1Spells = ["Bookspeak", "Charm Person", "Comprehend Languages / Obscure Languages", "Detect Magic",
                      "Detect Magic", "Enlarge / Reduce", "Faerie Fire", "Feather Fall", "Floating Disc",
                      "Hold Portal", "Identify", "Light / Darkness", "Magic Aura / Obscure Aura", "Magic Missile",
                      "Mending", "Message", "Shield", "Sleep", "Spider Climb", "Summon", "Unseen Servant"]
 
+DND3P5CoreWZ1Spells = []
 
-DND5CoreWZSpells = ["Alarm", "Burning Hands", "Charm Person", "Chromatic Orb", "Color Spray", "Comprehend Languages",
+DND5CoreWZ1Spells = ["Alarm", "Burning Hands", "Charm Person", "Chromatic Orb", "Color Spray", "Comprehend Languages",
                     "Detect Magic", "Disguise Self", "Expeditious Retreat", "False Life", "Feather Fall",
                     "Find Familiar", "Fog Cloud", "Grease", "Identify", "Illusory Script", "Jump", "Longstrider",
                     "Mage Armor", "Magic Missile", "Protection from Evil and Good", "Ray of Sickness", "Shield",
                     "Silent Image", "Sleep", "Tasha's Hideous Laughter", "Tenser's Floating Disk", "Thuderwave",
                     "Unseen Servant", "Witch Bolt"]
+
+DND5CoreSC1Spells = ""
 
 
 #
@@ -29,13 +32,13 @@ class SpellBookGenerators(commands.Cog):
         T_Return = "Read Magic, "
         T_DupProt = []
         for i in range(3):
-            T_DupProt.append(choice(LOTFPCoreMUSpells))
+            T_DupProt.append(choice(LOTFPCoreMU1Spells))
 
         while T_DupProt[0] == T_DupProt[1] or T_DupProt[0] == T_DupProt[2]:
-            T_DupProt[0] = choice(LOTFPCoreMUSpells)
+            T_DupProt[0] = choice(LOTFPCoreMU1Spells)
 
         while T_DupProt[1] == T_DupProt[2]:
-            T_DupProt[1] = choice(LOTFPCoreMUSpells)
+            T_DupProt[1] = choice(LOTFPCoreMU1Spells)
 
         for i in range(3):
             T_Return += T_DupProt[i]
@@ -45,4 +48,8 @@ class SpellBookGenerators(commands.Cog):
     #
     @commands.command(name="DnD5WZSpellBook", aliases=["5eWzSpellBook", "5eWizardSpellBook", "5thWizardSpellBook"])
     async def DnD5WZSpellBook(self, ctx):
-        await ctx.channel.send("5E Wizard spellbook method called")
+        T_Return = ""
+        T_DupProt = []
+        for i in range(6):
+            T_DupProt.append(choice(DND5CoreWZ1Spells))
+        await ctx.channel.send(T_Return)
