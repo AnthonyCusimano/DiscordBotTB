b4877d252f1a0817e10ac1540d7f711aacf69869
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-03-04 17:41:17 -0500

    Update ShopGenerator.py
    much closer!

--- ShopGenerator.py
+++ ShopGenerator.py
@@ -5,9 +5,13 @@ import requests
 class ShopGenerator:
 
     def __init__(self):
-        item = requests.get("https://www.dnd5eapi.co/api/spells/acid-arrow/")
-        # for todo_item in item.json():
-        #     print(todo_item["count"])
-        print(item.content["name"])
+        # item = requests.get("https://www.dnd5eapi.co/api/spells/acid-arrow/")
+        item = requests.get("https://www.dnd5eapi.co/api/equipment/")
+        # for todo_item in item:
+        #     print(todo_item)
+        # print(item.json()['results'])
+        for todo_lole in item.json()['results']:
+            print(todo_lole['name'])
+        # print(item.json()["name"])WORKS with acid arrow
         shopELO = 0
 
