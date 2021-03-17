d9e2c0aabd4152085a0d319a484296c3b6a854fc
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-03-16 22:54:05 -0400

    Update ShopGenerator.py
    DetermineMarkup method added

--- ShopGenerator.py
+++ ShopGenerator.py
@@ -4,37 +4,51 @@ import requests
 from discord.ext import commands
 
 
+#
+def DetermineMarkup():
+    markupTier = randrange(0, 100)
+    if markupTier < 30:
+        myMarkUp = 1
+    elif markupTier < 50:
+        myMarkUp = 1.25
+    elif markupTier < 70:
+        myMarkUp = 1.5
+    elif markupTier < 90:
+        myMarkUp = 2.0
+    elif markupTier < 95:
+        myMarkUp = 0.75
+
+    return myMarkUp
+
+
 # will create a DnD 5E shop using a score / points system
 # TODO blacksmith, general store, provisioner, alchemist, leatherworker
 class ShopGenerator(commands.Cog):
 
     def __init__(self, _debug=False):
 
-        myMarkup = 0.75
-        print(myMarkup)
-
         if _debug:
             # trying
-            myMarkupTier = randrange(0, 100)
-            print(myMarkupTier)
-            if myMarkupTier < 30:
+            self.myMarkupTier = randrange(0, 100)
+            print(self.myMarkupTier)
+            if self.myMarkupTier < 30:
                 myMarkUp = 1
-            elif myMarkupTier < 50:
+            elif self.myMarkupTier < 50:
                 myMarkUp = 1.25
-            elif myMarkupTier < 70:
+            elif self.myMarkupTier < 70:
                 myMarkUp = 1.5
-            elif myMarkupTier < 90:
+            elif self.myMarkupTier < 90:
                 myMarkUp = 2.0
-            elif myMarkupTier < 95:
+            elif self.myMarkupTier < 95:
                 myMarkupTier = 0.75
 
-            print(myMarkUp)
+            print(self.myMarkupTier)
             item = requests.get("https://www.dnd5eapi.co/api/equipment/")
             for todo_lole in item.json()['results']:
                 requester = 'https://www.dnd5eapi.co' + todo_lole['url']
                 lole = requests.get(requester)
                 coster = (lole.json()['cost']['quantity'])
-                costWMarkup = coster * myMarkUp
+                costWMarkup = coster * self.myMarkupTier
                 currancy = lole.json()['cost']['unit']
                 name = lole.json()['name']
                 print(name + " costs " + str(costWMarkup) + currancy)
@@ -44,12 +58,18 @@ class ShopGenerator(commands.Cog):
     #
     @commands.command(name="Blacksmith", aliases=["BS"])
     async def Blacksmith(self, ctx):
+
+        myMarkupTier = DetermineMarkup()
+        print(myMarkupTier)
+
         nextItemOdds = randrange(0, 50)
         nextItemOdds = 25
         if nextItemOdds > 24:
+            myMarkupTier
             item = requests.get("https://www.dnd5eapi.co/api/equipment/amulet")  # medium?
             await ctx.channel.send("The Blacksmith has an Amulet in stock, which costs {quan} {Sigurd}".format
-                                   (quan=item.json()['cost']['quantity'], Sigurd=item.json()['cost']['unit']))
+                                   (quan=float(item.json()['cost']['quantity']) * myMarkupTier,
+                                    Sigurd=item.json()['cost']['unit']))
         item = requests.get("https://www.dnd5eapi.co/api/equipment/ball-bearings-bag-of-1000")  # medium?
         item = requests.get("https://www.dnd5eapi.co/api/equipment/battleaxe")  # likely
         item = requests.get("https://www.dnd5eapi.co/api/equipment/breastplate")  # unlikely
