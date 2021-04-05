2eb10c5df5103c5c204f262a3cf54121f2721371
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-04-04 22:20:30 -0400

    Update TavernGenerator.py
    size catagories for Taverns added
    (Taverns are also inns fitting in with DnD slang)

--- TavernGenerator.py
+++ TavernGenerator.py
@@ -1,12 +1,37 @@
 from discord.ext import commands
+from random import randrange
 
 import NameGenerators
 
 
 class TavernGenerator(commands.Cog):
 
+    def getMyRoomsAvailable(self):
+        if self.mySize == 1:
+            # rooms available low
+            return randrange(0, 3)
+
+        elif self.mySize == 2:
+            return randrange(0, 5)
+
+        elif self.mySize == 3:
+            return randrange(1, 6)
+
+        elif self.mySize == 4:
+            return randrange(3, 14)
+
+        elif self.mySize == 5:
+            return randrange(6, 22)
+
+        elif self.mySize == 6:
+            return randrange(10, 30)
+
     @commands.command(name="TavernFull", aliases=["TavernComplete"])
     async def TavernFull(self, ctx):
         T_Name = NameGenerators.FormTavernName()
+        # TODO think about mySize
+        self.mySize = randrange(1, 6)
+        print(self.getMyRoomsAvailable())
+
         await ctx.channel.send(T_Name)
-        
+
