74603436c317a4cabd7a7e33ae8db64c1ef793b8
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-05-11 22:55:35 -0400

    Update OneShotGenerator.py
    "A small group of Orcs | Gnolls is attacking the village" is trash, but it's a start

--- OneShotGenerator.py
+++ OneShotGenerator.py
@@ -6,11 +6,25 @@ from discord.ext import commands
 
 #
 with open("OneShot.txt") as o:
-    shotLines = o.readLines()
+    shotLines = o.readlines()
 shotOne = shotLines[0].split(',')
 shotTwo = shotLines[1].split(',')
 shotOne[-1] = shotOne[-1][:-1]
 
+
 #
 def FormOneShot():
-    ""
+    T_Return = "A small group of "
+    T_Return += choice(shotOne)
+    T_Return += " has been attacking the village"
+
+    return T_Return
+
+
+#
+class OneShotGenerator(commands.Cog):
+
+    #
+    @commands.command(name="OneShot", aliases=["QuickAdventure"])
+    async def OneShot(self, ctx):
+        await ctx.channel.send(FormOneShot())
