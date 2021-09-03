9618941b0fbe31f7923eb9e8c64d3dc8e784abbe
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-09-02 20:23:57 -0400

    Add files via upload
    currently can only have on dungeon, and a dungeon is only a size, will probably make a version that uses the 5e DMG tables to create the dungeon

---
+++ DungeonGenerator.py
@@ -0,0 +1,25 @@
+from random import randrange
+
+from discord.ext import commands
+
+
+#
+class DungeonGenerator(commands.Cog):
+
+    def __init__(self):
+        # D10
+        sizeSeed = randrange(9)
+        # small dungeon
+        if sizeSeed < 4:
+            self.mySizeSeed = "small"
+        # medium dungeon
+        elif sizeSeed < 9:
+            self.mySizeSeed = "medium"
+        # large dungeon
+        else:
+            self.mySizeSeed = "large"
+
+    #
+    @ commands.command(name="randDungeon", aliases=["randomDungeon"])
+    async def randDungeon(self, ctx):
+        await ctx.channel.send(self.mySizeSeed)
