3b78ebe0ac22ae6e15f46a3a60d51e8ef82a1d55
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-09-06 16:36:06 -0400

    Update DnD5EDMGTables.py
    manument table is kinda cool I just don't like bell curve tables

--- DnD5EDMGTables.py
+++ DnD5EDMGTables.py
@@ -98,3 +98,38 @@ class FEDMGTables:
             return "secret door"
         elif T_DoorRando == 19:
             return "secret door; barred, stuck, or locked"
+        
+    #
+    def randomMonument(self):
+        T_MonuRando = random.randrange(19)
+
+        if T_MonuRando == 0:
+            T_Return = "Sealed burial mound or pyramid"
+        elif T_MonuRando == 1:
+            T_Return = "Plundered burial mound or pyramid"
+        elif T_MonuRando == 2:
+            T_Return = "Faced carved into a mountainside or cliff"
+        elif T_MonuRando == 3:
+            T_Return = "Giant statues carved out of a mountainside or cliff"
+        elif T_MonuRando < 6:
+            T_Return = "Intact obelisk etched with a warning, historical lore, or religious iconography"
+        elif T_MonuRando < 8:
+            T_Return = "Ruined or toppled obelisk"
+        elif T_MonuRando < 10:
+            T_Return = "Intact statue of a person or deity"
+        elif T_MonuRando < 13:
+            T_Return = "Ruined or toppled statue of a person or deity"
+        elif T_MonuRando == 13:
+            T_Return = "Great stone wall, intact, with tower fortifications spaced at one-mile intervals"
+        elif T_MonuRando == 14:
+            T_Return = "Great stone wall in ruins"
+        elif T_MonuRando == 15:
+            T_Return = "Great stone arch"
+        elif T_MonuRando == 16:
+            T_Return = "Fountain"
+        elif T_MonuRando == 17:
+            T_Return = "Intact circle of standing stones"
+        elif T_MonuRando == 18:
+            T_Return = "Ruined or toppled circle of standing stones"
+        elif T_MonuRando == 19:
+            T_Return = "totem pole"
