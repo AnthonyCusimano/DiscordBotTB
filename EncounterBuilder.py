9e869ec68652548923d9b965756937bd740f9cee
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-10-02 20:53:57 -0400

    Update EncounterBuilder.py
    still bad, but almost not the worst

--- EncounterBuilder.py
+++ EncounterBuilder.py
@@ -9,14 +9,38 @@ goblinHunterSlingPoints = 50
 numPlayers = 4
 playerLevel = 1
 
+
 #
 class EncounterBuilder(commands.Cog):
     """"""""
 
+    def pointCalculator(self):
+        return numPlayers * playerLevel
+
     @commands.command("goblinAmbush")
     async def goblinAmbush(self, ctx, _chaos=0):
 
-        # ambush gets additional points
+        # ambush gets additional points and are always deadly++
         T_myPointAdjustment = 200
+        T_myPointValue = 200 + self.pointCalculator()
+        T_myPointsRemaining = T_myPointValue
 
+        # horribly named number of blowguns
         T_numSlings = random.randrange(2, numPlayers)
+
+        # TODO meld adding units to an encounter and removing points from the encounter
+
+        T_myPointsRemaining -= T_numSlings * goblinHunterSlingPoints
+
+        T_Return = {}
+
+        for i in range(T_numSlings):
+            T_Return[-1] = "sling"
+
+        # all we have left is ankleshankers currently
+        while T_myPointsRemaining > 0:
+            T_Return[-1] = "ankleshanker"
+            T_myPointsRemaining -= goblinAnkleShankerPoints
+
+        return T_Return
+
