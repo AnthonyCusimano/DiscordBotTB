9efa3736038f64dc504665ec9ddd7bcb0d59d669
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-10-24 14:27:07 -0400

    Update Hex.py
    spent some time not seeing an issue with the fact I wasn't calling the HexTile constructor correctly

--- Hex.py
+++ Hex.py
@@ -1,12 +1,21 @@
 # hex neighbours laid out in order of N NE SE S SW NW
 class HexTile:
 
-    myColour = ""
+    # green, red, blue, yellow, magenta, tan, orange, ebony
+    # myColour = ""
 
+    # hills, mountain, grassland, forest, jungle, swamp, desert, tundra
+    myPrimaryBiome = ""
+
+    #
+    # mySecondaryBiome = ""
+
+    #
     def __init__(self, _neighbours):
 
         # TODO needs rando
         self.myColour = "green"
+        self.myPrimaryBiome = "grassland"
     #     for i in range(6):
     #         self.myNeighbors[-1] = _neighbours[i]
 
