d94577bcbadc29732bb36015a5ff5ce001026c96
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-10-28 17:39:54 -0400

    Update HexInterface.py
    passing ctx now

--- HexInterface.py
+++ HexInterface.py
@@ -11,12 +11,12 @@ class HexInterface(commands.Cog):
 
     #
     @commands.command(name="HexStartup")
-    async def HexStartup(self):
+    async def HexStartup(self, ctx):
         ""
         # currently unneeded cus HexGrid calls this in it's constructor
         # HexGrid.initGrid()
 
     #
     @commands.command(name="movePCs", aliases=["moveOnHex"])
-    async def movePCs(self, _direct):
-        HexInterface.theGrid.moveParty(_direct)
+    async def movePCs(self, ctx, _direct):
+        HexInterface.theGrid.moveParty(int(_direct))
