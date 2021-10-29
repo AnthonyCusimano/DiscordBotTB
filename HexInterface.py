d21e2e37a694efd6ff0feaaf7d6865e9dc5ff40d
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-10-28 23:55:13 -0400

    Update HexInterface.py
    can now check the current position of the PCs

--- HexInterface.py
+++ HexInterface.py
@@ -20,3 +20,9 @@ class HexInterface(commands.Cog):
     @commands.command(name="movePCs", aliases=["moveOnHex"])
     async def movePCs(self, ctx, _direct):
         HexInterface.theGrid.moveParty(int(_direct))
+
+    #
+    @commands.command(name="getCurrentPosition", aliases=["getOurCurrentPosition", "getPCCurrentPosition"])
+    async def getCurrentPosition(self, ctx):
+        await ctx.channel.send("The PCs are at hex: " + str(HexInterface.theGrid.playerPosition[0]) + ", " +
+                               str(HexInterface.theGrid.playerPosition[1]))
