32e86242e6489b6d597ba6f9ac1cb608c45a7ab0
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-11-04 22:39:24 -0400

    Update Hex.py
    starting to fill out the hex info

--- Hex.py
+++ Hex.py
@@ -1,21 +1,16 @@
+from random import randrange
+
 # hex neighbours laid out in order of N NE SE S SW NW
 class HexTile:
-
-    # green, red, blue, yellow, magenta, tan, orange, ebony
-    # myColour = ""
-
-    # hills, mountain, grassland, forest, jungle, swamp, desert, tundra
-    myPrimaryBiome = ""
-
-    #
-    # mySecondaryBiome = ""
 
     #
     def __init__(self, _neighbours):
 
         # TODO needs rando
         self.myColour = "green"
-        self.myPrimaryBiome = "grassland"
+        self.myPrimaryBiome = self.determineBiome()
+        self.myRiver = [-1]
+        self.myPosition = [0, 0]
     #     for i in range(6):
     #         self.myNeighbors[-1] = _neighbours[i]
 
@@ -26,6 +21,27 @@ class HexTile:
     #
     def getNeighborByID(self, _ID):
         return self.myNeighbors[_ID]
+
+    #
+    def determineBiome(self):
+        # TODO need detail on how to deterime a biome type
+        T_Rando = randrange(1, 8)
+        if T_Rando == 1:
+            return "forest"
+        elif T_Rando == 2:
+            return "grassland"
+        elif T_Rando == 3:
+            return "hills"
+        elif T_Rando == 4:
+            return "mountain"
+        elif T_Rando == 5:
+            return "swamp"
+        elif T_Rando == 6:
+            return "desert"
+        elif T_Rando == 7:
+            return "taiga"
+        elif T_Rando == 8:
+            return "lake"
 
     #
     # def assignAllNeighbors(self, _neighbors):
