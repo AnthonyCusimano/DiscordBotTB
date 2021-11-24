228ace87864d01a1feb43d86d9325e84211fef80
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-11-23 23:53:38 -0500

    Update EncounterBuilder.py
    getting back into the swing of things, hex crawling and encounter generation are now top priorities

--- EncounterBuilder.py
+++ EncounterBuilder.py
@@ -14,6 +14,7 @@ playerLevel = 1
 class EncounterBuilder(commands.Cog):
     """"""""
 
+    #
     def pointCalculator(self):
         return numPlayers * playerLevel
 
@@ -22,7 +23,7 @@ class EncounterBuilder(commands.Cog):
 
         # ambush gets additional points and are always deadly++
         T_myPointAdjustment = 200
-        T_myPointValue = 200 + self.pointCalculator()
+        T_myPointValue = T_myPointAdjustment + self.pointCalculator()
         T_myPointsRemaining = T_myPointValue
 
         # horribly named number of blowguns
@@ -43,4 +44,16 @@ class EncounterBuilder(commands.Cog):
             T_myPointsRemaining -= goblinAnkleShankerPoints
 
         await ctx.channel.send(T_Return)
+
+    @commands.command("roamingAnkheg")
+    async def roamingAnkheg(self, ctx):
+
+        T_MyPointAdjustment = 0
+        T_myPointValue = T_MyPointAdjustment + self.pointCalculator()
+
+        # TODO
+        
+    @commands.command("singleMonster")
+    async def singleMonster(self, HD=1):
+        ""
 
