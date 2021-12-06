6a2aaccdf27a4bb26617fd1aa1a5867759e95a9c
Anthony Cusimano <anthonycusimano91@hotmail.com>
2021-12-06 13:06:21 -0500

    Update HexInterface.py
    I hate myself enough to make a 1D array of hexes

--- HexInterface.py
+++ HexInterface.py
@@ -37,13 +37,32 @@ class HexInterface(commands.Cog):
         await ctx.channel.send("The PCs are at hex: " + str(HexInterface.theGrid.playerPosition[0]) + ", " +
                                str(HexInterface.theGrid.playerPosition[1]))
 
-    # TODO turbo broken lole
+    #
     @commands.command(name="getCurrentHexInfo", aliases=["currentHexInfo"])
     async def getCurrentHexInfo(self, ctx):
-        # TODO try Y*2*X = Y &&&&& X*2
+
         T_XFactor = HexInterface.theGrid.playerPosition[0] * 2.0
         T_YFactor = HexInterface.theGrid.playerPosition[1] * 2.0 * HexInterface.theGrid.mySizeMaxX
 
+        T_Return = "The current hex is a: " +\
+                   str(HexInterface.theGrid.myGrid[int(T_YFactor + T_XFactor)].myPrimaryBiome)
+
+        if HexInterface.theGrid.myGrid[int(T_YFactor + T_XFactor)].myRiver[0] == -1:
+            T_Return += ", This hex has no notable traversable river"
+
         T_currentHexAddress = HexInterface.theGrid.myGrid[int(T_YFactor + T_XFactor)]
-        await ctx.channel.send("The current hex is a: " +
-                               str(HexInterface.theGrid.myGrid[int(T_YFactor + T_XFactor)].myPrimaryBiome))
+        await ctx.channel.send(T_Return)
+
+    #
+    @commands.command(name="gridPrintDebug", aliases=["printGrid", "gridPrint"])
+    async def gridPrintDebug(self, ctx):
+
+        for _y in range(HexInterface.theGrid.mySizeY):
+            for _x in range(HexInterface.theGrid.mySizeMaxX):
+                T_Send = ""
+                T_Send = "The hex at position " + \
+                         str(HexInterface.theGrid.myGrid[int(_y + (_x * (_y+1)))].myPrimaryBiome)
+
+                # T_Send += " " +
+
+                await ctx.channel.send(T_Send)
