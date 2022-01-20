b71a6e0ff657b5086a21e9e830a2940f63551a0f
Anthony Cusimano <anthonycusimano91@hotmail.com>
2022-01-20 00:51:18 -0500

    Update Monster.py
    playing around more with default values in the constructor

--- Monster.py
+++ Monster.py
@@ -5,6 +5,6 @@ from Creature import Creature
 class Monster (Creature):
 
     #
-    def __init__(self, _name, _modifiers=[0, 0, 0, 0, 0, 0]):
+    def __init__(self, _name="", _modifiers=[0, 0, 0, 0, 0, 0], _hitpoints=1):
         #
-        Creature.__init__(_name, _modifiers)
+        Creature.__init__(self, _name, _modifiers, _hitpoints)
