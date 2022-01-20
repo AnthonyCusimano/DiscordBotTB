ed46c16752a1c57f55c259571c51685406f11a41
Anthony Cusimano <anthonycusimano91@hotmail.com>
2022-01-19 23:56:52 -0500

    Update Monster.py
    no overloading allowed :pensive: 
    Monsters are now Creatures

--- Monster.py
+++ Monster.py
@@ -1,19 +1,10 @@
+from Creature import Creature
 
 
 #
-class Monster:
-
-    # default / blank constructor
-    def __init__(self):
-        self.myName = ""
-        self.myAbilityScoreModifiers = [0, 0, 0, 0, 0, 0]
+class Monster (Creature):
 
     #
-    def __init__(self, _modifiers, _name):
-        self.myName = _name
-        self.myAbilityScoreModifiers = _modifiers.copy()
-
-    # copy constructor
-    # def __init__(self, _monster):
-    #    self.myName = _monster.name
-    #    self.myAbilityScoreModifiers = _monster.myAbilityScoreModifiers.copy()
+    def __init__(self, _name, _modifiers=[0, 0, 0, 0, 0, 0]):
+        #
+        Creature.__init__(_name, _modifiers)
