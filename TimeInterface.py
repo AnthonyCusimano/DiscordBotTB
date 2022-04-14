9fc2ae140d5014fbb8de245eba8a553e700678ba
Anthony Cusimano <anthonycusimano91@hotmail.com>
2022-04-13 23:43:16 -0400

    Update TimeInterface.py
    slight formatting improvement

--- TimeInterface.py
+++ TimeInterface.py
@@ -11,7 +11,7 @@ class TimeInterface(commands.Cog):
     #
     @commands.command(name="whatTimeIsIt", aliases=["time"])
     async def whatTimeIsIt(self, ctx):
-        # T_Post = "I'm working on it"
-        T_Post = str(tim.timeOfDay[0])
-        # T_Post = str(TimeInterface.tim.timeOfDay[0])
+
+        T_Post = "The current in game time is "
+        T_Post += str(tim.timeOfDay[0]) + ":" + str(tim.timeOfDay[1])
         await ctx.channel.send(T_Post)
