be909a07c717a3e007bb9ccc9fb55c4a115b5e76
Anthony Cusimano <anthonycusimano91@hotmail.com>
2022-06-28 23:13:58 -0400

    Update TimeInterface.py
    whatMonthIsIT added
    alias for whatTimeISIt added

--- TimeInterface.py
+++ TimeInterface.py
@@ -9,9 +9,18 @@ tim = Time()
 class TimeInterface(commands.Cog):
 
     #
-    @commands.command(name="whatTimeIsIt", aliases=["time"])
+    @commands.command(name="whatTimeIsIt", aliases=["time", "timeOfDay"])
     async def whatTimeIsIt(self, ctx):
 
         T_Post = "The current in game time is "
         T_Post += str(tim.timeOfDay[0]) + ":" + str(tim.timeOfDay[1])
+        await ctx.channel.send(T_Post)
+
+    #
+    @commands.command(name="whatMonthIsIt", aliases=["month"])
+    async def whatMonthIsIt(self, ctx):
+
+        T_Post = "The current month is "
+        T_Post += tim.getMonthName()
+
         await ctx.channel.send(T_Post)
