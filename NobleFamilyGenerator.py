6a980e9d373aa656d54721c7b32dc0733bda1f71
Anthony Cusimano <anthonycusimano91@hotmail.com>
2022-09-03 15:16:06 -0400

    Update NobleFamilyGenerator.py
    some comments added, mostly intent / TODO

--- NobleFamilyGenerator.py
+++ NobleFamilyGenerator.py
@@ -6,10 +6,12 @@ from NameGenerators import FormFirstName
 from random import randint
 
 
-#
+# currently just looking to generate a household consisting of members of one immediate family
+# eventually it'd be nice to have some maternal heritage thrown in as well
+# possibly eventually able to generate a family tree
 class NobleFamily:
 
-    #
+    # self.numChildren = 0
     def __init__(self):
         #
         self.numChildren = 0
@@ -25,15 +27,13 @@ class NobleFamily:
         T_Return = []
         print(self.numChildren)
 
+        # TODO protect T_Return from repeat names
         for x in range(self.numChildren):
             T_Return.append(FormFirstName())
             print(T_Return[-1])
 
         return T_Return
 
-
-
-
-    #
+    # yoinked from NameGenerators
     def determineFamilyName(self):
         return FormLastName()
