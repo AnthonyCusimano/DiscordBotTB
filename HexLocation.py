05b67a94d1c30a0d02c8fb0b048eb11eda997920
Anthony Cusimano <anthonycusimano91@hotmail.com>
2022-09-17 19:44:51 -0400

    Add files via upload
    going to be using this class to link the noble system to the hex crawl system, and finally bring the hex crawling to the next level of having real landmarks

---
+++ HexLocation.py
@@ -0,0 +1,17 @@
+
+
+#
+class HexLocationTypes:
+
+    #
+    def __init__(self, _ID):
+        print("xD")
+
+
+#
+class HexLocation:
+
+    #
+    def __init__(self):
+        self.myType = HexLocationTypes()
+
